d20711723f0886b0e80e929897c105f6d2058d42
Đorđe <the2922000@gmail.com>
2024-01-23 23:06:37 +0100

    Added google sign in functionality

---
+++ Poguzija/app.config.js
@@ -0,0 +1,51 @@
+export default {
+  expo: {
+    name: "Poguzija",
+    slug: "Poguzija",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.plaoludastruja.poguzija"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.plaoludastruja.poguzija",
+      googleServicesFile: process.env.GOOGLE_SERVICE_JSON
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      "@react-native-google-signin/google-signin"
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {
+        origin: false
+      },
+      eas: {
+        projectId: "990d7876-9a02-4fa0-9a71-75918414f1d9"
+      }
+    }
+  }
+}
--- Poguzija/app/(tabs)/fridge.tsx
+++ Poguzija/app/(tabs)/fridge.tsx
@@ -1,10 +1,106 @@
+import { Button, View, Text, StyleSheet, Image } from 'react-native';
 import BackgroundSafeAreaView from '../../components/BackgroundSafeAreaView';
-
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { useEffect, useState } from 'react';
+const isdev = false;
 
 export default function FridgeScreen() {
+    const [error, setError] = useState();
+    const [userInfo, setUserInfo] = useState();
+
+    useEffect(() => {
+        if(isdev){
+            GoogleSignin.configure({
+                webClientId: "679997496367-v24ck2ikahtou5jd89fa870fp9s83plt.apps.googleusercontent.com"
+            })
+        }
+        
+    }, [])
+
+    const signin = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const user = await GoogleSignin.signIn();
+            setUserInfo(user);
+            setError();
+        }catch (e) {
+            setError(e);
+        }
+    }
+
+    const logout = () => {
+        GoogleSignin.revokeAccess();
+        GoogleSignin.signOut();
+        setUserInfo();
+    }
+
     return (
         <BackgroundSafeAreaView>
-
+            {isdev && <View>
+                <Text>{JSON.stringify(error)}</Text>
+                {userInfo && <Text style={styles.buttonText}>{JSON.stringify(userInfo.user)}</Text>}
+                {userInfo && <Image source={{ uri: userInfo.user.photo }} style={styles.image} />}
+                { userInfo ? 
+                    (<Button title='Logout' onPress={logout} />) : 
+                    (<GoogleSigninButton size={GoogleSigninButton.Size.Standard} color={GoogleSigninButton.Color.Light} onPress={signin}/>)
+                }
+            </View>}
         </BackgroundSafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    image: {
+        width: 300,
+        height: 250,
+        borderRadius: 18,
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        marginBottom: 10,
+        paddingLeft: 10,
+    },
+    button: {
+        backgroundColor: 'blue',
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        marginTop: 10,
+        width: '100%'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    iconButton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 300,
+        height: 250,
+    },
+    iconButtonLabel: {
+        color: '#fff',
+        marginTop: 12,
+    },
+    flex: {
+        flex: 1,
+        width: '100%',
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
