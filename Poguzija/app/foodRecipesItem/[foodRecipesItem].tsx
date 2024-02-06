7ed6bbf471e3a58687ce8443157b0c680febe40c
Đorđe <the2922000@gmail.com>
2024-02-06 23:25:28 +0100

    Updated foodRecipsItem component

--- Poguzija/app/foodRecipesItem/[foodRecipesItem].tsx
+++ Poguzija/app/foodRecipesItem/[foodRecipesItem].tsx
@@ -1,12 +1,15 @@
 import { useLocalSearchParams } from 'expo-router';
 import BackgroundSafeAreaView from '../../components/BackgroundSafeAreaView';
-import React, { useEffect, useRef, useState } from 'react';
-import { View, TextInput, Pressable, Text, StyleSheet, Image, Platform, ScrollView, KeyboardAvoidingView } from 'react-native';
-import { FoodRecipes, StorageFolder } from '../../model/model';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { View, TextInput, Pressable, Text, StyleSheet, Image, Platform, ScrollView, KeyboardAvoidingView, Alert, Dimensions } from 'react-native';
+import { FoodRecipes, Ingredient, Step, StorageFolder } from '../../model/model';
 const PlaceholderImage = require('../../assets/images/icon.png');
 import Carousel from 'react-native-snap-carousel';
 import { COLORS, SIZES } from '../../constants/Colors';
-import { GetFoodRecipe } from '../../service/service';
+import {  GetFoodRecipe } from '../../service/service';
+import { MaterialIcons } from '@expo/vector-icons';
+import BottomSheet, { BottomSheetScrollView, BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import AddIngredientsModal from '../../components/AddIngredientsModal';
 
 
 export default function FoodRecipesItem() {
@@ -26,91 +29,173 @@ export default function FoodRecipesItem() {
             console.error('Error fetching data:', error);
         }
     }
+    
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
 
 
     const renderItem = ({ item }: { item: string }) => {
         return (
-            <Image source={{ uri: item }} style={styles.image} />
-
+            <View style={[styles.images, { width: screenWidth, height: 2 * screenHeight / 3 }]} >
+                <Image source={{ uri: item }} style={[styles.image, { width: screenWidth, height: 2 * screenHeight / 3 }]} />
+            </View>
         );
     };
+
     return (
         <BackgroundSafeAreaView>
+            <View style={styles.scrollViewContent}>
+                <View style={[styles.flex, { flexDirection: 'row'}]}>
+                    <Carousel
+                        data={food?.images}
+                        renderItem={renderItem}
+                        sliderWidth={screenWidth}
+                        itemWidth={screenWidth}
+                        layout="default"
+                    />
+                </View>
 
-                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollViewContent}>
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', }}>
-                        <Carousel
-                            data={ (food?.images) ? food?.images : []}
-                            renderItem={renderItem}
-                            sliderWidth={300} // Adjust the width as needed
-                            itemWidth={250}  // Adjust the item width as needed
-                            layout="default"
-                        />
-                    </View>
-                    {/*<Image source={imageSource} style={styles.image} />
-                    <Pressable style={styles.iconButton} onPress={pickImageAsync}>
-                        <MaterialIcons name="add-photo-alternate" size={24} color="black" />
-                    </Pressable>*/}
+                <BottomSheet
+                    snapPoints={useMemo(() => ['35', '65', '95'], [])}
+                    backgroundStyle={{ backgroundColor: COLORS.dark }}
+                >
+                    <BottomSheetScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollViewContent}>
+                        <Text style={styles.subtitleText}>Recipe name</Text>
+                        <View style={styles.inputContainer}>
+                            <MaterialIcons name="receipt" style={styles.icon} />
+                            <BottomSheetTextInput
+                                style={styles.textInput}
+                                placeholder="Recipe name"
+                                value={food?.title}
+                                autoComplete='off'
+                                editable={false}
+                            />
+                        </View>
 
-                    
-                </ScrollView>
+                        <Text style={styles.subtitleText}>Serving size</Text>
+                        <View style={styles.inputContainer}>
+                            <MaterialIcons name="people" style={styles.icon} />
+                            <BottomSheetTextInput
+                                style={styles.textInput}
+                                placeholder="Serving size"
+                                value={food?.servingSize}
+                                editable={false}
+                            />
+                        </View>
+
+                        <Text style={styles.subtitleText}>Ingredients</Text>
+                        {food?.ingredients?.map((ingredient, index) => (
+                            <View key={index} style={styles.ingredientItem}>
+                                <Text style={[styles.textInput, { width: "auto" }]}>   {ingredient.name}   -   {ingredient.amount} {ingredient.unit}</Text>
+                            </View>
+                        ))}
+
+                        <Text style={styles.subtitleText}>Cooking instructions</Text>
+                        {food?.steps?.map((step, index) => (
+                            <BottomSheetTextInput
+                                style={styles.input}
+                                multiline={true}
+                                value={`${step.number}. ${step.description}`}
+                                key={step.number}
+                                editable={false}
+                            />
+                        ))}
+                    </BottomSheetScrollView>
+                </BottomSheet>
+
+            </View>
         </BackgroundSafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-    },
-    input: {
-        width: '100%',
-        height: 40,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        marginBottom: 10,
-        paddingLeft: 10,
-    },
-    button: {
-        backgroundColor: 'blue',
-        borderRadius: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        marginTop: 10,
-        width: '100%'
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    image: {
-        width: 300,
-        height: 250,
-        borderRadius: 18,
-        marginTop: SIZES.extraLarge
-    },
-    iconButton: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 300,
-        height: 250,
-    },
-    iconButtonLabel: {
-        color: '#fff',
-        marginTop: 12,
-    },
     flex: {
         flex: 1,
         width: '100%',
+        justifyContent: 'center',
     },
     scrollViewContent: {
-        flexGrow: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    images: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: SIZES.extraLarge
+    },
+    image: {
+        borderTopLeftRadius: SIZES.extraLarge,
+        borderTopRightRadius: SIZES.extraLarge,
+        marginTop: SIZES.extraLarge
+    },
+    input: {
+        width: '95%',
+        minHeight: 60,
+        backgroundColor: COLORS.light,
+        borderRadius: SIZES.extraLarge,
+        marginBottom: SIZES.small,
+        padding: SIZES.small,
+        color: COLORS.tint,
+        fontSize: SIZES.large
+    },
+    button: {
+        textAlign: 'center',
+        justifyContent: 'center',
+        width: '85%',
+        backgroundColor: COLORS.tint,
+        borderRadius: SIZES.extraLarge,
+        padding: SIZES.base,
+        marginVertical: SIZES.base,
+        elevation: 2,
+    },
+    buttonText: {
+        color: COLORS.white,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: SIZES.large,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '95%',
+        height: 60,
+        backgroundColor: COLORS.light,
+        borderRadius: SIZES.extraLarge,
+        marginBottom: SIZES.small,
+        paddingHorizontal: SIZES.small,
+        color: COLORS.tint,
+        fontSize: SIZES.large,
+    },
+    textInput: {
+        width: '100%',
+        marginRight: 10,
+        color: COLORS.tint,
+        fontSize: SIZES.large,
+    },
+    icon: {
+        marginRight: 10,
+        color: COLORS.lightDark,
+        fontSize: SIZES.extraLarge
+    },
+    subtitleText: {
+        width: '85%',
+        color: COLORS.tint,
+        fontSize: SIZES.extraLarge,
+        fontWeight: 'bold',
+        marginBottom: SIZES.base,
+        marginTop: SIZES.small
+    },
+    ingredientItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '95%',
+        height: 60,
+        backgroundColor: COLORS.light,
+        borderRadius: SIZES.extraLarge,
+        marginBottom: SIZES.small,
+        paddingHorizontal: SIZES.small,
+        color: COLORS.tint,
+        fontSize: SIZES.large,
     },
 });
